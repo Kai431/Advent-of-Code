c28c3527922ad2c9f01dc5c707ed7562df88d8ff
Kai <kai.tinkhauser.htl@gmail.com>
2023-12-01 18:34:52 +0100

    Day1 Part1

--- 2023/Day1/Day1/Program.cs
+++ 2023/Day1/Day1/Program.cs
@@ -5,14 +5,8 @@ int sum = 0;
 while(!reader.EndOfStream)
 {
     string line = reader.ReadLine();
-    string num = string.Empty;
-    for(int i = 0; i < line.Length; i++)
-    {
-        if (char.IsNumber(line[i]))
-        {
-            num += line[i];
-        }
-    }
+    string nums = string.Concat(line.Where(Char.IsDigit));
+    string num = Convert.ToString(nums[0]) + Convert.ToString(nums[nums.Length-1]);
     sum += Convert.ToInt32(num);
 }
 
